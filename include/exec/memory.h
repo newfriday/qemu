3cfe46cbfe9877eb64b090cf98cfaaaf72c8f7a4
Hyman Huang(黄勇) <yong.huang@smartx.com>
2023-07-26 18:47:05 +0800

    cpr: Mismatched GPAs fix
    For cpr-exec mode, ramblock_is_ignored is always true, and the address of
    each migrated memory region must match the address of the statically
    initialized region on the target.  However, for a PCI rom block, the region
    address is set when the guest writes to a BAR on the source, which does not
    occur on the target, causing a "Mismatched GPAs" error during cpr-exec
    migration.
    
    To fix, unconditionally set the target's address to the source's address
    if the region does not have an address yet.
    
    Signed-off-by: Steve Sistare <steven.sistare@oracle.com>

--- include/exec/memory.h
+++ include/exec/memory.h
@@ -768,6 +768,7 @@ struct MemoryRegion {
     bool flush_coalesced_mmio;
     uint8_t dirty_log_mask;
     bool is_iommu;
+    bool has_addr;
     RAMBlock *ram_block;
     Object *owner;
     /* owner as TYPE_DEVICE. Used for re-entrancy checks in MR access hotpath */
@@ -2320,6 +2321,17 @@ void memory_region_set_enabled(MemoryRegion *mr, bool enabled);
  * @addr: new address, relative to container region
  */
 void memory_region_set_address(MemoryRegion *mr, hwaddr addr);
+
+/*
+ * memory_region_set_address_only: set the address of a region.
+ *
+ * Same as memory_region_set_address, but without causing transaction side
+ * effects.
+ *
+ * @mr: the region to be updated
+ * @addr: new address, relative to container region
+ */
+void memory_region_set_address_only(MemoryRegion *mr, hwaddr addr);
 
 /*
  * memory_region_set_size: dynamically update the size of a region.
